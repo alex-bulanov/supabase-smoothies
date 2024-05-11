bec92aa661a22a9842ab4251cb2ce7bebd01312e
alex-bulanov <alex-bulanov.mail@yandex.ru>
2024-05-11 16:40:06 +0300

    Извлечение данных

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,43 @@
 import supabase from '../config/supabaseClient';
+import { useEffect, useState } from 'react';
+import SmoothieCard from '../components/SmoothieCard';
 
 const Home = () => {
-  console.log('supabase', supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { error, data } = await supabase.from('smoothies').select();
+
+      if (error) {
+        setFetchError('Could not fetch the smoothies');
+        setSmoothies(null);
+        console.log('error', error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* oreder-by buttons*/}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
